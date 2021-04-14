329d59c08c51d3feed026602984c7c3617654a82
Jim Speakman <jim@jimspeakman.net>
2021-04-14 17:31:58 +0100

    Java 8

--- src/test/java/example/ApplicationTest.java
+++ src/test/java/example/ApplicationTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static java.util.Collections.emptyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -16,14 +18,14 @@ public class ApplicationTest {
 
     @Test
     void noResultReturnedWithNoData() {
-        var mockDataService = mock(DataService.class);
-        var application = new Application(mockDataService);
+        DataService mockDataService = mock(DataService.class);
+        Application application = new Application(mockDataService);
 
         when(mockDataService.getComments()).thenReturn(emptyList());
         when(mockDataService.getPosts()).thenReturn(emptyList());
         when(mockDataService.getUsers()).thenReturn(emptyList());
 
-        var results =  application.getUsersWithPopularPosts(0);
+        List<User> results =  application.getUsersWithPopularPosts(0);
 
         Assertions.assertTrue(results.isEmpty());
     }
